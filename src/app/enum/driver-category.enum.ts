ae0cb019d5a425e144dbc85174ac4ccd40a53276
Marte Valerio Falcone <marte.vfalcone@gmail.com>
2025-12-06 12:39:35 +0100

    #1 dev, CarModelEnum added, DriverCategoryEnum values changed according to handbook

--- src/app/enum/driver-category.enum.ts
+++ src/app/enum/driver-category.enum.ts
@@ -1,8 +1,7 @@
 // Values to be put in JSON configuration files
 export enum DriverCategoryEnum {
-  D = 1,
-  C = 2,
-  B = 3,
-  A = 4,
-  S = 5
+  BRONZE = 0,
+  SILVER = 1,
+  GOLD = 2,
+  PLATINUM = 3
 }
--- src/app/models/acc-entry-list.model.ts
+++ src/app/models/acc-entry-list.model.ts
@@ -10,13 +10,13 @@ export interface DriverInfo {
 
 export interface Entry {
   drivers: DriverInfo[];
-  raceNumber: number;
-  forcedCarModel: number;
+  raceNumber: number; // TODO validation between 1 and 998
+  forcedCarModel: number; // TODO validation between -1 and car model max value
   overrideDriverInfo: 0 | 1;
   isServerAdmin: 0 | 1;
   defaultGridPosition?: number;
-  ballastKg?: number;
-  restrictor?: number;
+  ballastKg?: number; // TODO validation between 0 and 100
+  restrictor?: number; // TODO validation between 0 and 20
   customCar?: string;
   overrideCarModelForCustomCar?: number;
 }
