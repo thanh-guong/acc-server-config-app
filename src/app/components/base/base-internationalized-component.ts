4a14c76c2d22435850df1d598a0dfe5d9ceb780a
Marte Valerio Falcone <marte.vfalcone@gmail.com>
2025-12-19 20:43:06 +0100

    base internationalized component now includes static labels

--- src/app/components/base/base-internationalized-component.ts
+++ src/app/components/base/base-internationalized-component.ts
@@ -1,7 +1,9 @@
 import { I18N_KEYS } from "../../../const/i18n-const";
+import { LABELS } from "../../../const/label-const";
 
 export class BaseInternationalizedComponent {
   I18N_KEYS = I18N_KEYS;
+  LABELS = LABELS;
 
   constructor() { }
 }
