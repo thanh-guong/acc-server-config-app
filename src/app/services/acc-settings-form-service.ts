12c656b60578f6e17052e3e2bb54b521680eb57a
Marte Valerio Falcone <marte.vfalcone@gmail.com>
2026-01-17 23:39:31 +0100

    #29 dev, validation on central entry path list form field (windows and unix style)

--- src/app/services/acc-settings-form-service.ts
+++ src/app/services/acc-settings-form-service.ts
@@ -6,7 +6,7 @@ import { CarGroupsEnum } from '../enum/car-groups.enum';
 import { FormationLapTypeEnum } from '../enum/formation-lap-type.enum';
 import { TrackMedalsRequirementEnum } from '../enum/track-medal-requirement.enum';
 import { ACC_SETTINGS_VALUES } from '../../const/form-values-const';
-import { integerPatternValidator } from '../validators/pattern-validators';
+import { fullPathValidator, integerPatternValidator } from '../validators/pattern-validators';
 
 @Injectable({ providedIn: 'root' })
 export class AccSettingsFormService extends BaseFormService<AccSettings, NonNullableFormBuilder> {
@@ -53,7 +53,9 @@ export class AccSettingsFormService extends BaseFormService<AccSettings, NonNull
       dumpLeaderboards: this.fb.control<0 | 1>(ACC_SETTINGS_VALUES.dumpLeaderboards.defaultValue as 0 | 1,Validators.required),
       isRaceLocked: this.fb.control<0 | 1>(ACC_SETTINGS_VALUES.isRaceLocked.defaultValue as 0 | 1,Validators.required),
       randomizeTrackWhenEmpty: this.fb.control<0 | 1>(ACC_SETTINGS_VALUES.randomizeTrackWhenEmpty.defaultValue as 0 | 1, Validators.required),
-      centralEntryListPath: this.fb.control<string | undefined>(ACC_SETTINGS_VALUES.centralEntryListPath.defaultValue),
+      centralEntryListPath: this.fb.control<string | undefined>(ACC_SETTINGS_VALUES.centralEntryListPath.defaultValue, [
+        fullPathValidator(),
+      ]),
       allowAutoDQ: this.fb.control<0 | 1>(ACC_SETTINGS_VALUES.allowAutoDQ.defaultValue as 0 | 1, Validators.required),
       shortFormationLap: this.fb.control<0 | 1>(ACC_SETTINGS_VALUES.shortFormationLap.defaultValue as 0 | 1, Validators.required),
       dumpEntryList: this.fb.control<0 | 1>(ACC_SETTINGS_VALUES.dumpEntryList.defaultValue as 0 | 1, Validators.required),
--- src/const/pattern-const.ts
+++ src/const/pattern-const.ts
@@ -1,4 +1,5 @@
 export const PATTERN_NAMES = {
     INTEGER: 'integerPattern',
     IPV4: 'ipv4',
+    FULL_PATH: 'fullPath',
 };
